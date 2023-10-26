959e74027923cd79b60b6efd50996316498104be
pvshake <pvshakeu@gmail.com>
2023-10-26 17:18:48 -0300

    Finalizado por enquanto ambiente de teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
+import { BASE_URL_API } from "../utils/constants";
 import * as faceapi from "face-api.js";
 
 export default function Home() {
@@ -25,7 +27,7 @@ export default function Home() {
     });
   }, [isWebCamOpen]);
 
-  const takePhoto = () => {
+  async function takePhoto() {
     const video = videoRef.current;
     const canvas = canvasRef.current;
 
@@ -55,12 +57,65 @@ export default function Home() {
       );
 
       const data = canvas.toDataURL("image/jpg");
-      const link = document.createElement("a");
-      link.download = "image.jpg";
-      link.href = data;
-      link.click();
+
+      document.getElementById("capturedImage").src = data;
     }
-  };
+  }
+
+  async function alignFace() {
+    const imageSrc = document.getElementById("capturedImage").src;
+    document.getElementById("alignedFaceImage").src = imageSrc;
+    const base64Image = imageSrc.split(",")[1];
+    const payload = { imagem_base64: base64Image };
+
+    try {
+      const response = await fetch(`${BASE_URL_API}/face/align`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+      const data = await response.json();
+      document.getElementById("alignedFaceOutput").value =
+        data.face_alinhada_base64;
+    } catch (error) {
+      console.error("Erro ao alinhar a face:", error);
+    }
+  }
+
+  async function validateFace() {
+    takePhoto(); // Captura uma nova imagem para validação
+    const probeImageBase64 = document.getElementById("alignedFaceOutput").value;
+    const matchImageSrc = document.getElementById("capturedImage").src;
+    document.getElementById("validatedFaceImage").src = matchImageSrc;
+    const matchImageBase64 = matchImageSrc.split(",")[1];
+
+    const payload = {
+      imagem_probe: {
+        imagem_base64: probeImageBase64,
+        alinhada: false,
+      },
+      imagem_match: {
+        imagem_base64: matchImageBase64,
+        alinhada: false,
+      },
+    };
+
+    try {
+      const response = await fetch(`${BASE_URL_API}/face/validate`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+      const data = await response.json();
+      document.getElementById("validationOutput").value = data.score;
+    } catch (error) {
+      console.error("Erro ao validar as faces:", error);
+    }
+  }
 
   useEffect(() => {
     if (isWebCamOpen) {
@@ -111,33 +166,17 @@ export default function Home() {
     };
   }, []);
 
-  const toggleWebcam = () => {
-    setLoading(true);
-
-    if (streaming && isWebCamOpen) {
-      streaming.getTracks().forEach((track) => {
-        track.stop();
-      });
-      setIsWebCamOpen(false);
-      setStreaming(null);
-      setLoading(false);
-    } else {
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-          setIsWebCamOpen(true);
-          setStreaming(stream);
-        })
-        .catch((error) => {
-          console.error("Erro ao acessar a câmera:", error);
-          setLoading(false);
-        });
-    }
-  };
-
   return (
     <main className={styles.page}>
-      <div className="main-container">
+      <div
+        className="main-container"
+        style={{
+          width: "100vw",
+          display: "flex",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+        }}
+      >
         <div
           className="card"
           style={{
@@ -172,6 +211,45 @@ export default function Home() {
                   borderRadius: "12px",
                 }}
               />
+              <img
+                style={{ display: "none" }}
+                id="capturedImage"
+                src=""
+                alt="Imagem Capturada"
+              ></img>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    marginTop: 16,
+                  }}
+                >
+                  <label style={{ color: "#e2e8f0", fontSize: 14 }}>
+                    Base 64 - Alinhamento
+                  </label>
+                  <textarea
+                    id="alignedFaceOutput"
+                    style={{ width: "400px", height: "100px" }}
+                  ></textarea>
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    marginTop: 16,
+                  }}
+                >
+                  <label style={{ color: "#e2e8f0", fontSize: 14 }}>
+                    Score - Reconhecimento
+                  </label>
+                  <textarea
+                    id="validationOutput"
+                    readOnly
+                    style={{ width: "200px" }}
+                  ></textarea>
+                </div>
+              </div>
             </>
           ) : (
             <>
@@ -313,25 +391,98 @@ export default function Home() {
                 >
                   {loading ? "Ligando..." : "Ligar Câmera"}
                 </button>
-                <button
-                  style={{
-                    backgroundColor: "#e2e8f0",
-                    color: "#1a202c",
-                    fontFamily: "sans-serif",
-                    fontSize: 16,
-                    fontWeight: 600,
-                    borderRadius: 12,
-                    height: 40,
-                    width: 200,
-                    margin: 8,
-                    border: "none",
-                  }}
-                  onClick={() => console.log("oiiiiiii")}
-                >
-                  ⬆️ Fazer upload
-                </button>
               </>
             )}
+          </div>
+        </div>
+        <div
+          className="right-side-container"
+          style={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: 32,
+          }}
+        >
+          <div
+            className="align-content"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: 16,
+            }}
+          >
+            <img
+              id="alignedFaceImage"
+              style={{
+                width: 360,
+                height: 280,
+                border: "2px solid #e2e8f0",
+                borderRadius: 12,
+              }}
+              src="./default-avatar.png"
+              alt="Primeira Imagem"
+            ></img>
+            <button
+              onClick={alignFace}
+              style={{
+                backgroundColor: "#e2e8f0",
+                color: "#1a202c",
+                fontFamily: "sans-serif",
+                fontSize: 16,
+                fontWeight: 600,
+                borderRadius: 12,
+                height: 40,
+                width: 200,
+                cursor: "pointer",
+                border: "none",
+              }}
+              disabled={loading || buttonDisabled}
+            >
+              Alinhar primeira foto
+            </button>
+          </div>
+          <div
+            className="recognize-content"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: 16,
+            }}
+          >
+            <img
+              id="validatedFaceImage"
+              style={{
+                width: 360,
+                height: 280,
+                border: "2px solid #e2e8f0",
+                borderRadius: 12,
+              }}
+              src="./default-avatar.png"
+              alt="Segunda Imagem"
+            ></img>
+            <button
+              onClick={validateFace}
+              style={{
+                backgroundColor: "#e2e8f0",
+                color: "#1a202c",
+                fontFamily: "sans-serif",
+                fontSize: 16,
+                fontWeight: 600,
+                borderRadius: 12,
+                height: 40,
+                width: 200,
+                margin: 8,
+                cursor: "pointer",
+                border: "none",
+              }}
+              disabled={loading || buttonDisabled}
+            >
+              Fazer reconhecimento
+            </button>
           </div>
         </div>
       </div>
